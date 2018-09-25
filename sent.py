920c618c2f70d9c7d29c8abd77222a9705380d15
Keerttik Titan <34683060+Keerttik@users.noreply.github.com>
2018-09-25 07:13:23 +0530

    Create sent.py

---
+++ sent.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,request,jsonify
+import tweepy
+from textblob import TextBlob
+
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+public_tweets = api.search('Ronaldo is the best')
+
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+    
+    #Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+print("")
